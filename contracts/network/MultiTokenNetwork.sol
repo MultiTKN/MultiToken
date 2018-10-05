9fdb2be721c2b4a2323b55de809a79b11f64db39
Mikhail Melnik <by.zumzoom@gmail.com>
2018-10-05 09:41:24 +0300

    fix codestyle in network

--- contracts/network/MultiTokenNetwork.sol
+++ contracts/network/MultiTokenNetwork.sol
@@ -11,59 +11,71 @@ contract MultiTokenNetwork is Pausable {
     event NewMultitoken(address indexed mtkn);
     event NewDeployer(uint256 indexed index, address indexed oldDeployer, address indexed newDeployer);
 
-    address[] public multitokens;
-    mapping(uint256 => AbstractDeployer) public deployers;
+    address[] public _multitokens;
+    mapping(uint256 => AbstractDeployer) public _deployers;
+
+    function multitokens(uint256 i) public view returns(address) {
+        return _multitokens[i];
+    }
 
     function multitokensCount() public view returns(uint256) {
-        return multitokens.length;
+        return _multitokens.length;
     }
 
     function allMultitokens() public view returns(address[]) {
-        return multitokens;
+        return _multitokens;
+    }
+
+    function deployers(uint256 i) public view returns(AbstractDeployer) {
+        return _deployers[i];
+    }
+
+    function deployersCount() public view returns(uint256) {
+        return _deployers.length;
     }
 
     function allWalletBalances(address wallet) public view returns(uint256[]) {
-        uint256[] memory balances = new uint256[](multitokens.length);
-        for (uint i = 0; i < multitokens.length; i++) {
-            balances[i] = ERC20(multitokens[i]).balanceOf(wallet);
+        uint256[] memory balances = new uint256[](_multitokens.length);
+        for (uint i = 0; i < _multitokens.length; i++) {
+            balances[i] = ERC20(_multitokens[i]).balanceOf(wallet);
         }
         return balances;
     }
 
     function deleteMultitoken(uint index) public onlyOwner {
-        require(index < multitokens.length, "deleteMultitoken: index out of range");
-        if (index != multitokens.length - 1) {
-            multitokens[index] = multitokens[multitokens.length - 1];
+        require(index < _multitokens.length, "deleteMultitoken: index out of range");
+        if (index != _multitokens.length - 1) {
+            _multitokens[index] = _multitokens[_multitokens.length - 1];
         }
-        multitokens.length -= 1;
+        _multitokens.length -= 1;
     }
 
     function disableBundlingMultitoken(uint index) public onlyOwner {
-        IBasicMultiToken(multitokens[index]).disableBundling();
+        IBasicMultiToken(_multitokens[index]).disableBundling();
     }
 
     function enableBundlingMultitoken(uint index) public onlyOwner {
-        IBasicMultiToken(multitokens[index]).enableBundling();
+        IBasicMultiToken(_multitokens[index]).enableBundling();
     }
 
     function disableChangesMultitoken(uint index) public onlyOwner {
-        IMultiToken(multitokens[index]).disableChanges();
+        IMultiToken(_multitokens[index]).disableChanges();
     }
 
     function setDeployer(uint256 index, AbstractDeployer deployer) public onlyOwner whenNotPaused {
         require(deployer.owner() == address(this), "setDeployer: first set MultiTokenNetwork as owner");
-        emit NewDeployer(index, deployers[index], deployer);
-        deployers[index] = deployer;
+        emit NewDeployer(index, _deployers[index], deployer);
+        _deployers[index] = deployer;
     }
 
     function deploy(uint256 index, bytes data) public whenNotPaused {
-        address mtkn = deployers[index].deploy(data);
-        multitokens.push(mtkn);
+        address mtkn = _deployers[index].deploy(data);
+        _multitokens.push(mtkn);
         emit NewMultitoken(mtkn);
     }
 
-    function makeCall(address _target, uint256 _value, bytes _data) public onlyOwner {
+    function makeCall(address target, uint256 value, bytes data) public onlyOwner {
         // solium-disable-next-line security/no-call-value
-        require(_target.call.value(_value)(_data), "Arbitrary call failed");
+        require(target.call.value(value)(data), "Arbitrary call failed");
     }
 }
